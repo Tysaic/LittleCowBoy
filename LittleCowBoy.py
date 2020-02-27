4defd9b4a0c6309f2f993e38759b3d5602e9922e
Tysaic <mendezlinaresi@gmail.com>
2020-02-27 20:32:41 -0300

    Installed nmap3 because is easier, Built scanner nmap as a class. After built itself more complex

--- LittleCowBoy.py
+++ LittleCowBoy.py
@@ -2,10 +2,15 @@ from modules.local_info import *
 
 list_ports = ('80', '22')
 if __name__ == '__main__':
-    ip = 'localhost'
+    ip = '127.0.0.1'
     port = 80
-    for x in list_ports:
-        print(scan_host_manually(ip, int(x) ) )
-
-    print(nmap_scan_host(ip, list_ports))
-
+    scan = Scanner(ip, port)
+    """
+    print("Machine Info: ", Scanner.get_machine_info())
+    print(scan.scan_host_manually() )
+    list_nmap = scan.basic_nmap_scan()
+    print(list_nmap)
+    print(Scanner("google.com").dns_nmap_scan())
+    print(scan.os_detection_nmap_scan())
+    """
+    print(scan.scanning_techniques())
--- modules/local_info.py
+++ modules/local_info.py
@@ -1,35 +1,55 @@
 import platform
-import requests
 import socket
-import nmap
+import nmap3
+import json
 
-def get_machine_info():
-    data = dict()
-    data['machine'] = platform.machine()
-    data['version'] = platform.version()
-    data['platform'] = platform.platform()
-    data['uname'] = platform.uname()
-    data['system'] = platform.system()
-    data['processor'] = platform.processor()
-    return data
 
-def scan_host_manually(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        code = sock.connect_ex((ip, port))
-        if code == 0:
-            return True
-        else:
+class Scanner:
+    def __init__(self, host, port=80):
+        self.nm = nmap3.Nmap()
+        self.nm_scan_tech = nmap3.NmapScanTechniques()
+        self.host = host
+        self.port = port
+    
+    @classmethod
+    def get_machine_info(cls):
+        data = dict()
+        data['machine'] = platform.machine()
+        data['version'] = platform.version()
+        data['platform'] = platform.platform()
+        data['uname'] = platform.uname()
+        data['system'] = platform.system()
+        data['processor'] = platform.processor()
+        return data
+
+    def scan_host_manually(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            code = sock.connect_ex((self.ip, self.port))
+            if code == 0:
+                return True
+            else:
+                return False
+        except:
             return False
-    except:
-        return False
-    finally:
-        sock.close()
+        finally:
+            sock.close()
 
-def nmap_scan_host(host, ports):
+    def basic_nmap_scan(self):
+        results = self.nm.scan_top_ports(self.host)
+        return results
 
-    nm = nmap.PortScanner()
-    flag = ' '
-    ports = flag.join(ports)
-    nm.scan(host, arguments='-n -sP -PE -PA{}'.format(ports))
-    print(nm.all_hosts())
+    def dns_nmap_scan(self):
+        results = self.nm.nmap_dns_brute_script(self.host)
+        return results
+
+    def os_detection_nmap_scan(self):
+        result = self.nm.nmap_os_detection(self.host)
+        return result
+
+    def identify_service_version_detection(self):
+        result = self.nm.nmap_version_detection(self.host)
+        return result
+    def scanning_techniques(self):
+        return self.nm_scan_tech.nmap_fin_scan(self.host)
+        
